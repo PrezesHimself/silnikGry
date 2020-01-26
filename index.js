000c45e016f143c53bf3b11f161bde6a3f9773b6
PrezesHimself <mateuszrorat@gmail.com>
2020-01-26 17:04:36 +0100

    normal maps attempt

--- index.js
+++ index.js
@@ -7,13 +7,21 @@
 			var camera, scene, renderer;
 			var uniforms;
       var stats = new Stats();
+      var images = []
 
       var mouseX = 0;
       var mouseY = 0;
 
         document.querySelector('#container').appendChild( stats.domElement );
+
+          loadImages([
+    'https://cdnb.artstation.com/p/assets/images/images/001/146/575/large/ulrick-wery-tileableset2-soil.jpg?1441028621'
+  ], function(_images) {
+      images = _images;
 			init();
 			animate();
+
+  })
 
 			function init() {
 
@@ -22,18 +30,39 @@
 				scene = new THREE.Scene();
 				var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
 
+
+      var canvas = document.getElementById('background');
+      canvas.width  = window.innerWidth;
+      canvas.height  = window.innerHeight;
+      var context = canvas.getContext('2d');
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = 70;
+
+      // console.log(images)      
+      context.fillStyle = context.createPattern(images[0], 'repeat');;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
 				uniforms = {
 					time: { value: 1.0 },
           resolution: { type: "v2", value: new THREE.Vector2() },
           mouse: { type: "v2", value: new THREE.Vector2() },
+          colorTexture: { value: new THREE.TextureLoader().load( 'https://cdnb.artstation.com/p/assets/images/images/001/146/575/large/ulrick-wery-tileableset2-soil.jpg?1441028621' ) },
+          normalMap: {
+            value: new THREE.TextureLoader().load('https://i.ibb.co/cYkGf9V/Normal-Map.png')
+          }
 				};
+  
+uniforms[ "colorTexture" ].value.wrapS = uniforms[ "colorTexture" ].value.wrapT = THREE.RepeatWrapping;
 
+uniforms[ "normalMap" ].value.wrapS = uniforms[ "normalMap" ].value.wrapT = THREE.RepeatWrapping;
 
 				var material = new THREE.ShaderMaterial( {
 					uniforms: uniforms,
 					vertexShader: document.getElementById( 'vertexShader' ).textContent,
 					fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 				} );
+
 
 				var mesh = new THREE.Mesh( geometry, material );
 				scene.add( mesh );
@@ -71,3 +100,38 @@
 				  requestAnimationFrame( animate );
         }, 1000/fps)
 			}
+
+
+function loadImage(url, callback) {
+  var image = new Image();
+    requestCORSIfNotSameOrigin(image, url)
+  image.src = url;
+  image.onload = callback;
+  return image;
+}
+
+function loadImages(urls, callback) {
+  var images = [];
+  var imagesToLoad = urls.length;
+
+  // Called each time an image finished
+  // loading.
+  var onImageLoad = function() {
+    --imagesToLoad;
+    // If all the images are loaded call the callback.
+    if (imagesToLoad === 0) {
+      callback(images);
+    }
+  };
+
+  for (var ii = 0; ii < imagesToLoad; ++ii) {
+    var image = loadImage(urls[ii], onImageLoad);
+    images.push(image);
+  }
+}
+
+function requestCORSIfNotSameOrigin(img, url) {
+  if ((new URL(url)).origin !== window.location.origin) {
+    img.crossOrigin = "";
+  }
+}
